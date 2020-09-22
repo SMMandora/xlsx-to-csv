33ea8b02daeab82a782a7b5000e9b9c61a6d843c
SMMandora <shubhammandora6@gmail.com>
2020-09-22 12:37:55 +0530

    updated

--- Exceltocsv.py
+++ Exceltocsv.py
@@ -1,31 +1,24 @@
-import pandas as pd
 import os
-import openpyxl as opx
 import csv
 import xlrd
 
-path = 'F:\pyprogs'
+path = '<Path to folder where .xlsx files are stored>'
 filelist = os.listdir(path)
-
-def selectsheet(filename):
-    wb = opx.load_workbook(filename)
-    sheets = wb.sheetnames
-    for sheetname in sheets:
-        sheet = wb[sheetname]
-        if (sheet['A11'] == 'Who is prime minister of india?'):
-             break
-    return sheetname
-
 
 for filename in filelist:
     if filename.endswith('.xlsx'):
-        sheetname = selectsheet(filename)
-
+        print('Working on file: {}'.format(filename))
         wb = xlrd.open_workbook(filename)
-        filename = filename.replace('.xlsx', '.csv')
-        sh = wb.sheet_by_name(sheetname)
-        csvfile = open(filename, 'w')
-        wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        sheets = wb.sheet_names()
+        for sheet in sheets :
+            sh = wb.sheet_by_name(sheet)
+            csvfilename = sheet+'.csv'
+            csvfile = open(csvfilename, 'w')
+            wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
-        for rownum in range(11,sh.nrows):
-            wr.writerow(sh.row_values(rownum))
+            for rownum in range(0,sh.nrows):
+                wr.writerow(sh.row_values(rownum))
+
+            print('{0} created from sheet in {1} '.format(csvfilename,filename))
+        print('File {} converted to CSV'.format(filename))
+
